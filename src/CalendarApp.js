6ec89ba48c64bf1509ce981391fe4f24d44a9946
Camilo López <camiloj.lopez79@gmail.com>
2022-04-24 15:08:03 -0400

    Inicio del proyecto - MERN Calendar

---
+++ src/CalendarApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {AppRouter} from "./router/AppRouter";
+
+function CalendarApp() {
+  return (
+		<>
+      <AppRouter /> 
+		</>
+  );
+}
+
+export default CalendarApp;
---
+++ src/router/AppRouter.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { 
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes
+} from "react-router-dom";
+import { CalendarScreen } from '../components/calendar/CalendarScreen'
+import { LoginScreen } from '../components/auth/LoginScreen'
+
+export const AppRouter = () => {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <CalendarScreen /> } />
+        <Route path='/login' element={ <LoginScreen /> } />
+        
+        <Route path='/*' element={ <Navigate to='/' replace /> } />
+      </Routes>
+    </BrowserRouter>
+  );
+}
